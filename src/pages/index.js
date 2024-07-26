bc1e68a944079ca16e078b4e121aa81ae2741db9
808vita <its808vita@gmail.com>
2024-07-26 15:03:42 +0530

    simple frontend component | api route for gemini call

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import GeminiPrompt from "@/components/GeminiPrompt";
+
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    ></main>
+    <div>
+      <GeminiPrompt />
+    </div>
   );
 }
